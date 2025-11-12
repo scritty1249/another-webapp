a61d3086304185f192eae624097a9074c641db63
Kyle <kaisonpham@gmail.com>
2025-11-11 22:21:45 -0800

    Updated version number

--- client/script/main.js
+++ client/script/main.js
@@ -1,4 +1,4 @@
 const overlayEl = document.getElementById("overlay");
 window.addEventListener("load", function() {
-    document.getElementById("version").textContent = "v0.0.4"
+    document.getElementById("version").textContent = "v0.0.45"
 });
