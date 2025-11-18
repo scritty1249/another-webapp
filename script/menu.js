cdb84e83163db16b816aa222b28b12d760b43f2b
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2025-11-18 11:58:21 +0000

    Update gh-pages to output generated at 39f9b4e

--- script/menu.js
+++ script/menu.js
@@ -1,10 +1,17 @@
 // main pause menu
 const menuPath = "../source/menu/"
 const menuButtonName = (type) => `menu-${type}-button.png`;
+const stopPropagation = (el) => el.addEventListener("click", function (event) {
+    event.stopPropagation();
+});
 
 const Menu = {
     createMenu: function (
-        destroyMenuCallback = () => {}, ...elements
+        destroyMenuCallback = () => {},
+        topLeft = [],
+        topRight = [],
+        bottomLeft = [],
+        bottomRight = []
     ) {
         const wrapper = document.createElement("div");
         wrapper.id = "menu";
@@ -12,18 +19,42 @@ const Menu = {
             event.preventDefault();
             destroyMenuCallback();
         });
-
-        elements.forEach(el => {
-            el.addEventListener("click", function (event) {
-                event.stopPropagation();
-            });
-            wrapper.appendChild(el)
+        
+        const sections = {
+            tl: document.createElement("div"),
+            tr: document.createElement("div"),
+            bl: document.createElement("div"),
+            br: document.createElement("div")
+        };
+        sections.tl.classList.add("top-left");
+        sections.tr.classList.add("top-right");
+        sections.bl.classList.add("bottom-left");
+        sections.br.classList.add("bottom-right");
+        wrapper.appendChild(sections.tl);
+        wrapper.appendChild(sections.tr);
+        wrapper.appendChild(sections.bl);
+        wrapper.appendChild(sections.br);
+        topLeft.forEach(el => {
+            stopPropagation(el)
+            sections.tl.appendChild(el)
+        });
+        topRight.forEach(el => {
+            stopPropagation(el)
+            sections.tr.appendChild(el)
+        });
+        bottomLeft.forEach(el => {
+            stopPropagation(el)
+            sections.bl.appendChild(el)
+        });
+        bottomRight.forEach(el => {
+            stopPropagation(el)
+            sections.br.appendChild(el)
         });
         return wrapper;
     },
-    createButton: function (buttonType = "blank", classes = [], events = {}) {
+    createButton: function (buttonType = "blank", events = {}) {
         const el = document.createElement("div");
-        el.classList.add("pointer-events", "button", "no-bg", ...classes);
+        el.classList.add("pointer-events", "button", "no-bg");
         // original dimensions 500x500 px
         el.style.backgroundImage = `url("${menuPath + menuButtonName(buttonType)}")`
         el.style.width = "calc(var(--unit) * 20)";
