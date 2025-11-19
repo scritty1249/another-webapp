4032ce045a789a18267bc335ef261bfa7bbe04bb
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2025-11-19 00:57:22 +0000

    Update gh-pages to output generated at fa80b9e

--- script/main.js
+++ script/main.js
@@ -2,3 +2,7 @@ const overlayEl = document.getElementById("overlay");
 window.addEventListener("load", function() {
     document.getElementById("version").textContent = "v0.0.45"
 });
+window.addEventListener("resize", function (event) {
+    document.body.height = window.innerHeight;
+    document.body.width = window.innerWidth;
+});
