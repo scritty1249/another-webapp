e79ee54db89c5177ddf344c88a57c8e3d31a5fab
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2025-11-24 09:21:24 +0000

    Update gh-pages to output generated at bb67d5c

--- script/cursor.js
+++ script/cursor.js
@@ -84,7 +84,7 @@ export function Mouse(
                 );
         });
         this.canvasElement.addEventListener("contextmenu", function(event) {
-            self.updatePosition(event);s
+            self.updatePosition(event);
             event.preventDefault();
         });
     }
