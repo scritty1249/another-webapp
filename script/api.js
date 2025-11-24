cf4268549c41a164c8027e19d3ea4dfaccc37fff
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2025-11-24 01:27:43 +0000

    Update gh-pages to output generated at 6ce96d1

--- script/api.js
+++ script/api.js
@@ -1,8 +1,8 @@
 import { API_ENDPOINT } from "./endpoint.js";
 
-function buildURL (path, params = {}, cookieJar = {}) {
+function buildURL (path, params = undefined, cookieJar = undefined) {
     let url = API_ENDPOINT;
-    url += "?path=" + path.replace("\\", ".");
+    url += "?path=" + path.replaceAll("/", ".");
     if (cookieJar)
         Object.entries(cookieJar).forEach(([key, value]) => {
             url += `&cookie-${key}=${value}`;
@@ -11,15 +11,20 @@ function buildURL (path, params = {}, cookieJar = {}) {
         Object.entries(params).forEach(([key, value]) => {
             url += `&${key}=${value}`;
         });
-    
+    return url;
 }
 
-function sendRequest (path, params = {}, method = "GET", body = {}, cookieJar = {}) {
+function sendRequest (path, params = {}, method = "GET", body = undefined, cookieJar = undefined) {
     const url = buildURL(path, params, cookieJar);
-    const data = { method: method.toUpperCase() };
+    const data = {
+        method: method.toUpperCase(),
+        redirect: "follow",
+        headers: {
+            "Content-Type": "text/plain;charset=utf-8"
+        }
+    };
     if (body) {
         // [!] Google Apps Script does not expose the request body for GET requests. Use POST if sending a request body!
-        data.headers = { "Content-Type": "application/json" };
         data.body = JSON.stringify(body);
     }
     Logger.debug(`[API] | ${method.toUpperCase()} Request to "${path}"`);
@@ -32,7 +37,7 @@ function sendRequest (path, params = {}, method = "GET", body = {}, cookieJar = 
         }).then(data => {
             if (data?.error)
                 Logger.error(
-                    `[API] | Server returned error ${data.error.code} ${data.error?.detail ? ": "+ data.error.detail : ""}`
+                    `[API] | Server returned error ${data.error.code}${data.error?.detail ? ": "+ data.error.detail : ""}`
                 );
             else
                 return data;
@@ -55,6 +60,20 @@ export function getOwnBase (sessionToken) {
 }
 
 export function refreshSession (sessionToken) {
-    return sendRequest("/game/load", {}, "GET", {}, {session: sessionToken})
+    return sendRequest("/api/refresh", {}, "GET", {}, {session: sessionToken})
         .then(data => data?.token.expires);
+}
+
+export function saveGame (sessionToken, backdrop, layout, ...currency) {
+    return sendRequest("/game/save", {}, "POST", {
+        game: {
+            backdrop: backdrop,
+            layout: layout
+        },
+        bank: {
+            cash: currency[0],
+            crypto: currency[1]
+        }
+    }, {session: sessionToken})
+        .then(data => Boolean(data?.success));
 }
