6832fe03b0d4789581a62f31f461dadca1fa8793
scritty1249 <84257126+scritty1249@users.noreply.github.com>
2025-12-17 01:03:20 +0000

    Update gh-pages to output generated at 1d96d77

--- script/utils.js
+++ script/utils.js
@@ -4,9 +4,11 @@ import * as THREE from "three";
 const b64RegPattern =
     /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
 const backgroundPath = (backgroundName) => `./source/bg/${backgroundName}/`;
-const defaultBackgroundColor = new THREE.Color(0x010101);
+const darkBg = new THREE.Color(0x010101);
+const lightBg = new THREE.Color(0xdddddd);
 
-function nestedSetEquals (set1, set2) { // [!] only compares to a depth of 2
+function nestedSetEquals(set1, set2) {
+    // [!] only compares to a depth of 2
     for (const item1 of set1) {
         let found = false;
         for (const item2 of set2) {
@@ -15,8 +17,7 @@ function nestedSetEquals (set1, set2) { // [!] only compares to a depth of 2
                 break;
             }
         }
-        if (!found)
-            return false;
+        if (!found) return false;
     }
     for (const item2 of set2) {
         let found = false;
@@ -26,35 +27,41 @@ function nestedSetEquals (set1, set2) { // [!] only compares to a depth of 2
                 break;
             }
         }
-        if (!found)
-            return false;
+        if (!found) return false;
     }
     return true;
 }
 
-export function loadBackgroundTexture (background, scene, loadingBg = defaultBackgroundColor) {
-    scene.background = loadingBg;
-    return loadTextureCube(backgroundPath(background), ".png")
-        .then((tex) => {
-            scene.background = tex;
-            scene.userData.background = background;
-            return true;
-        });
+export function loadBackgroundTexture(
+    background,
+    scene,
+    loadingBg = undefined
+) {
+    scene.background = loadingBg
+        ? loadingBg
+        : background.endsWith("dark")
+        ? darkBg
+        : lightBg;
+    return loadTextureCube(backgroundPath(background), ".png").then((tex) => {
+        scene.background = tex;
+        scene.userData.background = background;
+        return true;
+    });
 }
 
-export function loadAudio (src, ctx) {
+export function loadAudio(src, ctx) {
     return fetch(src)
-        .then(resp => resp.arrayBuffer())
-        .then(buffer => ctx.decodeAudioData(buffer));
+        .then((resp) => resp.arrayBuffer())
+        .then((buffer) => ctx.decodeAudioData(buffer));
 }
 
-export function banksEqual (me, them) {
+export function banksEqual(me, them) {
     const myKeys = Object.keys(me);
     const theirKeys = Object.keys(them);
     return (
         new Set(myKeys).symmetricDifference(new Set(theirKeys)).size === 0 &&
-        myKeys.every(key => me[key] == them[key])
-    )
+        myKeys.every((key) => me[key] == them[key])
+    );
 }
 
 export function download(filename, text) {
@@ -154,35 +161,41 @@ export function bindProtoProperties(object, target) {
     );
 }
 
-export function layoutsEqual(thisLayout, thatLayout) { // [!] currently, does not evaluate position changes. not sure if I want to implement that since the nodes float around passively...
+export function layoutsEqual(thisLayout, thatLayout) {
+    // [!] currently, does not evaluate position changes. not sure if I want to implement that since the nodes float around passively...
     try {
         const thisNodes = thisLayout.layout.nodes;
         const thatNodes = thatLayout.layout.nodes;
-        const thisNeighbors = new Set(thisLayout.layout.neighbors.map(edge => new Set(edge)));
-        const thatNeighbors = new Set(thatLayout.layout.neighbors.map(edge => new Set(edge)));
+        const thisNeighbors = new Set(
+            thisLayout.layout.neighbors.map((edge) => new Set(edge))
+        );
+        const thatNeighbors = new Set(
+            thatLayout.layout.neighbors.map((edge) => new Set(edge))
+        );
         const thisNodeTypes = {};
         const thatNodeTypes = {};
 
-        thisNodes.forEach(({type, position}) => {
-            if (!thisNodeTypes[type])
-                thisNodeTypes[type] = 1;
-            else
-                thisNodeTypes[type] += 1;
+        thisNodes.forEach(({ type, position }) => {
+            if (!thisNodeTypes[type]) thisNodeTypes[type] = 1;
+            else thisNodeTypes[type] += 1;
         });
-        thatNodes.forEach(({type, position}) => {
-            if (!thatNodeTypes[type])
-                thatNodeTypes[type] = 1;
-            else
-                thatNodeTypes[type] += 1;
+        thatNodes.forEach(({ type, position }) => {
+            if (!thatNodeTypes[type]) thatNodeTypes[type] = 1;
+            else thatNodeTypes[type] += 1;
         });
         return (
             thisLayout.background == thatLayout.background &&
             thisNodes.length == thatNodes.length &&
             nestedSetEquals(thisNeighbors, thatNeighbors) &&
-            Object.entries(thisNodeTypes).every(([type, count]) => thatNodeTypes[type] == count) &&
-            Object.entries(thatNodeTypes).every(([type, count]) => thisNodeTypes[type] == count)
+            Object.entries(thisNodeTypes).every(
+                ([type, count]) => thatNodeTypes[type] == count
+            ) &&
+            Object.entries(thatNodeTypes).every(
+                ([type, count]) => thisNodeTypes[type] == count
+            )
         );
-    } catch { // if a layout is missing an expected property, it means it's not a Layout. which likely means its NOT equal to whatever we're trying to compare anyways.
+    } catch {
+        // if a layout is missing an expected property, it means it's not a Layout. which likely means its NOT equal to whatever we're trying to compare anyways.
         Logger.info("failed to compare layouts");
         return false;
     }
@@ -190,7 +203,9 @@ export function layoutsEqual(thisLayout, thatLayout) { // [!] currently, does no
 
 export function layoutToJsonObj(scene, nodeManager) {
     const data = {
-        background: scene.userData?.background ? scene.userData.background : DEFAULT.BG,
+        background: scene.userData?.background
+            ? scene.userData.background
+            : DEFAULT.BG,
         layout: {
             nodes: [],
             neighbors: [],
@@ -200,14 +215,11 @@ export function layoutToJsonObj(scene, nodeManager) {
     nodeManager.nodelist.forEach((node, i) => {
         const posData = node.position.clone().round();
         data.layout.nodes.push(
-            new NodeObject(node.userData.type, `${i}`, [
-                    posData.x,
-                    posData.y,
-                    posData.z,
-                ],
-                node.userData?.exportData
-                ? node.userData.exportData
-                : {}
+            new NodeObject(
+                node.userData.type,
+                `${i}`,
+                [posData.x, posData.y, posData.z],
+                node.userData?.exportData ? node.userData.exportData : {}
             )
         );
         newIds[node.uuid] = i;
@@ -229,11 +241,11 @@ export function getStoredCurrencyFromLayout(layoutObj) {
     // [!] may be redundant, but it seems wasteful to init a brand new NodeManager just for this...
     const bank = {
         cash: 0,
-        crypto: 0
+        crypto: 0,
     };
     layoutObj.layout.nodes.forEach((node) => {
         if (node._data?.store)
-            bank[node._data.store.type] += node._data.store.amount
+            bank[node._data.store.type] += node._data.store.amount;
     });
     return bank;
 }
@@ -259,21 +271,24 @@ export function layoutFromJsonObj(jsonObj, scene, dragControls, nodeManager) {
                 Logger.error(error);
                 res = loadBackgroundTexture(DEFAULT.BG, scene);
             }
-        return res
-            .then(_ => {
-                jsonObj.layout.nodes.forEach((node) => {
-                    const newId = nodeManager.createNode(node.type, node.position, node._data);
-                    newIds[node.uuid] = newId;
-                });
-                jsonObj.layout.neighbors.forEach((tether) =>
-                    nodeManager.tetherNodes(newIds[tether[0]], newIds[tether[1]])
+        return res.then((_) => {
+            jsonObj.layout.nodes.forEach((node) => {
+                const newId = nodeManager.createNode(
+                    node.type,
+                    node.position,
+                    node._data
                 );
-                // update references
-                dragControls.objects = nodeManager.nodelist;
-                nodeManager.centerNodes();
-                Logger.debug("Loaded layout: ", jsonObj);
-                return true;
-            })
+                newIds[node.uuid] = newId;
+            });
+            jsonObj.layout.neighbors.forEach((tether) =>
+                nodeManager.tetherNodes(newIds[tether[0]], newIds[tether[1]])
+            );
+            // update references
+            dragControls.objects = nodeManager.nodelist;
+            nodeManager.centerNodes();
+            Logger.debug("Loaded layout: ", jsonObj);
+            return true;
+        });
     } catch (error) {
         Logger.error(`Error loading layout: `, jsonObj);
         Logger.error(error);
@@ -301,7 +316,7 @@ export function getRandomItems(array, count) {
     const selected = new Set();
     while (selected.size < count)
         selected.add(Math.floor(Math.random() * array.length));
-    return Array.from(selected, idx => array[idx]);
+    return Array.from(selected, (idx) => array[idx]);
 }
 
 export async function getClipboardText() {
@@ -329,10 +344,7 @@ export const _DebugTool = {
         const layoutData = layoutToJson(scene, nodeManager, false);
         const domData = document.documentElement.outerHTML;
         Logger.log("Generating debug file for download");
-        download(
-            `CUBE_GAME-${new Date().toISOString()}.log`,
-            logger.history
-        );
+        download(`CUBE_GAME-${new Date().toISOString()}.log`, logger.history);
     },
     marker: function (
         scene = undefined,
@@ -356,7 +368,9 @@ export const _DebugTool = {
                         .clone()
                         .sub(direction.clone().multiplyScalar(lineLength)),
                 ]);
-                const linemat = new THREE.LineBasicMaterial({ color: markerColor });
+                const linemat = new THREE.LineBasicMaterial({
+                    color: markerColor,
+                });
                 const line = new THREE.Line(linegeo, linemat);
                 marker.attach(line);
             }
@@ -371,7 +385,7 @@ export const _DebugTool = {
         raycaster,
         lineLength = 1,
         markerColor = 0x00ff00,
-        markerRadius = 0.05,
+        markerRadius = 0.05
     ) {
         return _DebugTool.marker(
             scene,
